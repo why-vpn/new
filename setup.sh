45efacef8ac4f6ea86f6ebf5a676fbde9ce5df63
why-vpn <91131537+why-vpn@users.noreply.github.com>
2021-09-22 09:45:15 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-IZIN=$( curl https://raw.githubusercontent.com/ADITYAH2/halucok/main/ipvps | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/why-vpn/premium/main/ip/ipvps.txt | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
